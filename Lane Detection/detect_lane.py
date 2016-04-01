b5c741ce568a8bd0b23ac83a068a062e6bdcf3dc
Sheifali Subir Khare <shiboo18@gmail.com>
2016-04-01 17:11:14 -0400

    Lane Detection
    Lane Detection Code

---
+++ Lane Detection/detect_lane.py
@@ -0,0 +1,25 @@
+from detect_lane_houghlines import *
+from detect_lane_fitline import *
+from detect_lane_houghlinesP import *
+from config_lane import *
+
+gb_lane_cfg = LaneCfg
+
+def test():
+    """ test lane detection """
+    global gb_lane_cfg
+
+    #capture = cv2.VideoCapture('data/road.avi')
+
+    # register to proc manager
+    proc = {    'fitline'      : detect_lane_over_fitline,
+                'houghlinesP'   : detect_lane_over_houghlinesP,
+                'houghlines'    : detect_lane_over_houghlines
+            }
+
+    while True:
+        img =  cv2.imread('road4.jpg',1)
+        proc[gb_lane_cfg['set']['proc']](gb_lane_cfg['set']['proc'], img, gb_lane_cfg)
+
+if __name__ == '__main__':
+    test()
